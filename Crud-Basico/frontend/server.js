25b4d30c7089ced8d8e481297050a5deaca40312
soficuozzo <2313251@ucc.edu.ar>
2025-10-20 10:53:46 -0300

    Tests funcionando localmente

--- Crud-Basico/frontend/server.js
+++ Crud-Basico/frontend/server.js
@@ -1,17 +1,20 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
+const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`Frontend running on http://localhost:${PORT}`);
 });
+
+
+
+
+
 
 
